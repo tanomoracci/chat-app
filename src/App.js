9cb4c30a2fac71c75142f3aaba348af0bfce61ee
Pablo Moracci <tano@quierostandup.com>
2020-08-17 19:27:24 -0300

    exercise n3 finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import Contact from './Contact';
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <>
-      <Contact avatar='https://randomuser.me/api/portraits/men/63.jpg' name='Roger Lewis' online={true} />
-      <Contact avatar='https://randomuser.me/api/portraits/men/41.jpg' name='Charles Coleman' online={false} />
-      <Contact avatar='https://randomuser.me/api/portraits/men/97.jpg' name='Antonio Robertson' online={true}/>
+    <ContactList />
+     
     </> 
   );
 }
